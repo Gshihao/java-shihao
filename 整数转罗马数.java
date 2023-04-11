b2dbbb6d957201b26ce2e6fae5ed7f020338dd4b
Gshihao <100885352+Gshihao@users.noreply.github.com>
2023-04-11 10:34:25 +0800

    Create 整数转罗马数.java

---
+++ 整数转罗马数.java
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public String intToRoman(int num) {
+
+        // 把阿拉伯数字与罗马数字可能出现的所有情况和对应关系，放在两个数组中，并且按照阿拉伯数字的大小降序排列
+
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int index = 0;
+
+        while (index < 13) {
+
+            // 特别注意：这里是等号
+
+            while (num >= nums[index]) {
+
+                stringBuilder.append(romans[index]);
+
+                num -= nums[index];
+
+            }
+
+            index++;
+
+        }
+
+        return stringBuilder.toString();
+
+    }
+
+}
